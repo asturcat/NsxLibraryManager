895d0396ffacb38f2d5930ccebe81508cabf5611
asturcat <167415915+asturcat@users.noreply.github.com>
2026-08-31 00:03:09 +0200

    Refactor update version retrieval in TitleLibraryService

--- src/NsxLibraryManager/Services/TitleLibraryService.cs
+++ src/NsxLibraryManager/Services/TitleLibraryService.cs
@@ -843,11 +843,13 @@ public class TitleLibraryService(
                 if (parentTitle is not null)
                 {
                     parentTitle.OwnedUpdates = updateCount;
-                    var maxUpdate = _nsxLibraryDbContext.Titles
+                    var updates = _nsxLibraryDbContext.Titles
                         .Where(x => x.ContentType == TitleContentType.Update &&
                                     x.OtherApplicationId == libraryTitle.OtherApplicationId)
-                        .Max(x => (int?)x.Version) ?? 0;
-                    parentTitle.LatestOwnedUpdateVersion = maxUpdate;
+                        .ToList();
+                    parentTitle.LatestOwnedUpdateVersion = updates.Count > 0 
+                        ? updates.Max(x => x.Version) 
+                        : 0;
                 }
                 break;
             }
